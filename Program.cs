af8cf992e37de85e643b84260577b7f7e24f42f0
vieira-andre <andre.vieira@outlook.com>
2019-07-12 21:59:40 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace migration_pair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
