b337addbeeb4b348355a911acdb46e45b6c958d9
vieira-andre <andre.vieira@outlook.com>
2019-07-12 23:27:37 -0300

    Add Cassandra's cluster and session

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Cassandra;
 
 namespace migration_pair
 {
@@ -7,6 +7,9 @@ namespace migration_pair
         private static readonly string[] endpoints = { "localhost" };
         private static readonly string keyspace = null, tableName = null;
         private static readonly string filePath = null;
+
+        private static Cluster cluster = Cluster.Builder().AddContactPoints(endpoints).Build();
+        private static readonly ISession session = cluster.Connect();
 
         static void Main(string[] args)
         {
