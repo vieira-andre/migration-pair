8c6e3f3177a01ab9138481e0fa6b6a4f313b9b55
vieira-andre <andre.vieira@outlook.com>
2019-07-12 23:23:10 -0300

    Add variables for endpoints, keyspace, table name and file path

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace migration_pair
 {
     class Program
     {
+        private static readonly string[] endpoints = { "localhost" };
+        private static readonly string keyspace = null, tableName = null;
+        private static readonly string filePath = null;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
