db179c605351d1358ac3c999188b5920bd97f057
vieira-andre <andre.vieira@outlook.com>
2019-07-15 06:21:02 -0300

    Add ColumnName property to CField class

--- Program.cs
+++ Program.cs
@@ -134,11 +134,13 @@ namespace migration_pair
     internal class CField
     {
         public dynamic Value { get; set; }
+        public string ColumnName { get; set; }
         public Type DataType { get; set; }
 
-        public CField(dynamic value, Type dataType)
+        public CField(dynamic value, string columnName, Type dataType)
         {
             Value = value;
+            ColumnName = columnName;
             DataType = dataType;
         }
     }
