91d01aa9aa6ead9f9c3e084e2323fa00f3087bbe
vieira-andre <andre.vieira@outlook.com>
2019-07-15 09:26:01 -0300

    [Re]create CColumn class

--- Program.cs
+++ Program.cs
@@ -139,4 +139,16 @@ namespace migration_pair
             DataType = dataType;
         }
     }
+
+    internal class CColumn
+    {
+        public string Name { get; set; }
+        public Type DataType { get; set; }
+
+        public CColumn(string name, Type dataType)
+        {
+            Name = name;
+            DataType = dataType;
+        }
+    }
 }
