c797e0a05214544e424d881acf639de3b4f06928
vieira-andre <andre.vieira@outlook.com>
2019-07-16 17:17:25 -0300

    Fix format and change UtcNow by Now

--- Helper/Logger.cs
+++ Helper/Logger.cs
@@ -6,14 +6,14 @@ namespace migration_pair.Helper
 {
     internal static class Logger
     {
-        private const string dateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssK";
+        private const string dateTimeFormat = "yyyy-MM-ddTHH:mm:ss.fffzzz";
         private static readonly string logFilePath = ConfigurationManager.AppSettings["Log_File_Path"];
 
         internal async static void Write(string message)
         {
             _ = Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
 
-            await File.AppendAllTextAsync(logFilePath, string.Concat(DateTime.UtcNow.ToString(dateTimeFormat), " >> ", message, Environment.NewLine));
+            await File.AppendAllTextAsync(logFilePath, string.Concat(DateTime.Now.ToString(dateTimeFormat), " >> ", message, Environment.NewLine));
         }
     }
 }
