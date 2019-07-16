520c6265beef6c37707617a7fecf6150a3a828d2
vieira-andre <andre.vieira@outlook.com>
2019-07-16 17:10:49 -0300

    Make datetime format a constant and include a new line at the end of the logged msg

--- Helper/Logger.cs
+++ Helper/Logger.cs
@@ -6,13 +6,14 @@ namespace migration_pair.Helper
 {
     internal static class Logger
     {
+        private const string dateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ssK";
         private static readonly string logFilePath = ConfigurationManager.AppSettings["Log_File_Path"];
 
         internal async static void Write(string message)
         {
             _ = Directory.CreateDirectory(Path.GetDirectoryName(logFilePath));
 
-            await File.AppendAllTextAsync(logFilePath, string.Concat(DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ssK"), " >> ", message));
+            await File.AppendAllTextAsync(logFilePath, string.Concat(DateTime.UtcNow.ToString(dateTimeFormat), " >> ", message, Environment.NewLine));
         }
     }
 }
