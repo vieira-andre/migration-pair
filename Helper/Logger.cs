1f1c422bb8d1cb670d0efe2edc7c2582af223157
vieira-andre <andre.vieira@outlook.com>
2019-07-16 17:03:57 -0300

    Create logger helper class

---
+++ Helper/Logger.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+
+namespace migration_pair.Helper
+{
+    internal static class Logger
+    {
+        private static readonly string logFilePath = ConfigurationManager.AppSettings["Log_File_Path"];
+
+        internal async static void Write(string message)
+        {
+
+        }
+    }
+}
