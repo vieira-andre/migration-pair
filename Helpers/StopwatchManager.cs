f3553aeb0b480007f7d782c27b38e9c94152e6a3
vieira-andre <andre.vieira@outlook.com>
2020-03-30 04:19:12 -0300

    Create StopwatchManager class

---
+++ Helpers/StopwatchManager.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace migration_pair.Helpers
+{
+    internal static class StopwatchManager
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        internal static Stopwatch Start()
+        {
+            return Stopwatch.StartNew();
+        }
+
+        internal static void StopAndLog(this Stopwatch stopwatch, [CallerMemberName]string methodMeasured = null)
+        {
+            stopwatch.Stop();
+            Logger.Info($"Time elapsed ({methodMeasured}): {stopwatch.Elapsed.Hours}h:{stopwatch.Elapsed.Minutes}m:{stopwatch.Elapsed.Seconds}s:{stopwatch.Elapsed.Milliseconds}ms.");
+        }
+    }
+}
