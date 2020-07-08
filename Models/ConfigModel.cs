3e6bb5eb49d926978ea69f1e7547b1ab9c87ec5e
vieira-andre <andre.vieira@outlook.com>
2020-07-08 03:31:14 -0300

    Create ConfigModel class

---
+++ Models/ConfigModel.cs
@@ -0,0 +1,40 @@
+﻿namespace Mycenae.Models
+{
+    public class ConfigModel
+    {
+        private int insertionBatch;
+
+        public TaskToPerform TaskToPerform { get; set; }
+        public Connections Connections { get; set; }
+        public DataFiles Files { get; set; }
+        public int InsertionBatch { get => insertionBatch; set => insertionBatch = (value > 0) ? value : 100000; }
+    }
+
+    public class Connections
+    {
+        public Connection Source { get; set; }
+        public Connection Target { get; set; }
+    }
+
+    public class DataFiles
+    {
+        public DataFile Extraction { get; set; }
+        public DataFile Insertion { get; set; }
+    }
+
+    public class Connection
+    {
+        private int port;
+
+        public string[] Endpoints { get; set; }
+        public int Port { get => port; set { port = (value > 0) ? value : Cassandra.ProtocolOptions.DefaultPort; } }
+        public string Keyspace { get; set; }
+        public string Table { get; set; }
+    }
+
+    public class DataFile
+    {
+        public string Path { get; set; }
+        public bool HasHeader { get; set; }
+    }
+}
